4e966e38c70cbf9dad22006dd1a82e3675a7605e
khinradanarpyaephyo98 <khinradanarpyaephyo98@gmail.com>
2023-08-14 23:49:37 +0630

    update

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask, render_template,request
 app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+   return render_template('home.html')
 
-@app.route('/', methods=['GET'])
-def api_endpoint():
-    # Process the request and call the actual API
-    # Return the response to Google Apps Script
-    response_data = {"message": "Hello from Python API"}
-    return jsonify(response_data)
+@app.route('/result',methods=['POST','GET'])
+def result():
+   output = request.form.to_dict()
+   question = output["question"]
+   print(question)
+   return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
