650437513b1ad46ef0bf4e123ba8062a4393253d
khinradanarpyaephyo98 <khinradanarpyaephyo98@gmail.com>
2023-08-21 15:10:56 +0630

    update the reply api

--- app.py
+++ app.py
@@ -9,79 +9,75 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain import HuggingFaceHub
 
 
+documents = []
+for file in os.listdir('docs'):
+   if file.endswith('.pdf'):
+      pdf_path = './docs/' + file
+      loader = PyPDFLoader(pdf_path)
+      documents.extend(loader.load())
+
+print("Number of documents :",len(documents))
+print(documents)
+
+
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=10)
+documents = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] ="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"
+llm=HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0, "max_length":512})
+chain = load_qa_chain(llm, chain_type="stuff")
+
 
 app = Flask(__name__)
  
 @app.route('/',methods=['GET'])
-def home():
-   
+def home():  
    return render_template('home.html')
 
-@app.route('/reply',methods=['POST','GET'])
+#for line api test
+@app.route('/reply', methods=['POST','GET'])
 def reply():
-   output = request.form.to_dict()
-   query = output["question"]
-   print( query)
+    data = request.get_json() # msg from the GAS
+    msg_from_gas = data.get('message')
+    print(" Received msg from the GAS : ",msg_from_gas)
+    docs = db.similarity_search(msg_from_gas)  # call AI model
+    print("Answer :",chain.run(input_documents=docs, question=msg_from_gas),"\n")
+    global answer1
+    answer1=chain.run(input_documents=docs, question=msg_from_gas)
+    return answer1
 
-   documents = []
-   for file in os.listdir('docs'):
-      if file.endswith('.pdf'):
-         pdf_path = './docs/' + file
-         loader = PyPDFLoader(pdf_path)
-         documents.extend(loader.load())
-
-   print("Number of documents :",len(documents))
-
-
-   text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
-   documents = text_splitter.split_documents(documents)
-
-   embeddings = HuggingFaceEmbeddings()
-   db = FAISS.from_documents(documents, embeddings)
-
-   os.environ["HUGGINGFACEHUB_API_TOKEN"] ="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"
-   llm=HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0, "max_length":512})
-   chain = load_qa_chain(llm, chain_type="stuff")
-
-   
-   docs = db.similarity_search(query)
-   print("Answer :",chain.run(input_documents=docs, question=query),"\n")
-   answer=chain.run(input_documents=docs, question=query)
-   return render_template('home.html',answer=answer,question=query)
-
-    
-   
-
+#for browser test
 @app.route('/result',methods=['POST','GET'])
 def result():
    output = request.form.to_dict()
-   question = output["question"]
-   print(question)
-   return render_template('home.html',question=question)
+   query = output["question"]
+   print( query)
+   docs = db.similarity_search(query)
+   print("Answer :",chain.run(input_documents=docs, question=query),"\n")
+   global answer2
+   answer2=chain.run(input_documents=docs, question=query)
+   return render_template('home.html',answer=answer2,question=query)
 
-@app.route('/line',methods=['GET'])
-def line():
-   
+
+@app.route('/check_reply',methods=['GET'])
+def check_reply():
    data = {
-        'message': 'This is the message from the API route!'
+        'answer1': answer1
     }
    return jsonify(data)
-   
-
-@app.route('/receive')
-def display_message():
-    message = "Hello, this is the message from the /receive route!"
-    return render_template('home.html', message=message)
 
 
-@app.route('/receive_frm_line', methods=['POST'])
-def receive_message():
-    data = request.get_json()
-    line_msg = data.get('message')
-    print(line_msg,"line msg ")
-    return render_template('home.html', line_msg=line_msg)
-
+@app.route('/check_result',methods=['GET'])
+def check_result():
+   data = {
+        'answer2': answer2
+    }
+   return jsonify(data)  
 
 
 if __name__ == '__main__':
    app.run()
+   
